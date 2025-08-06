9677502141eaab59005300a8ae7baffd6f488078
Syed Muhammad Ali <muhammadali200416@gmail.com>
2025-08-06 20:11:30 +0500

    Fix radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -29,7 +29,7 @@ cart.forEach((cartProduct) => {
             ${matchProduct.name}
           </div>
           <div class="product-price">
-            $${matchProduct.priceCents / 100}
+            $${(matchProduct.priceCents / 100).toFixed(2)}
           </div>
           <div class="product-quantity">
             <span>
@@ -51,7 +51,7 @@ cart.forEach((cartProduct) => {
           <div class="delivery-option">
             <input type="radio" checked
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${productID}">
             <div>
               <div class="delivery-option-date">
                 Tuesday, June 21
@@ -64,7 +64,7 @@ cart.forEach((cartProduct) => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${productID}">
             <div>
               <div class="delivery-option-date">
                 Wednesday, June 15
@@ -77,7 +77,7 @@ cart.forEach((cartProduct) => {
           <div class="delivery-option">
             <input type="radio"
               class="delivery-option-input"
-              name="delivery-option-1">
+              name="delivery-option-${productID}">
             <div>
               <div class="delivery-option-date">
                 Monday, June 13
